f5c2dc96ad45b0730b08133cb2f137fa23059175
ah3rzi <kanzi.csbku@gmail.com>
2025-11-02 16:44:23 +0300

    Displaying Platform icons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CanceledError } from "axios";
 
 const apiClient = axios.create({
     baseURL : 'https://api.rawg.io/api',
@@ -8,3 +8,5 @@ const apiClient = axios.create({
 })
 
 export default apiClient;
+
+export {CanceledError};
